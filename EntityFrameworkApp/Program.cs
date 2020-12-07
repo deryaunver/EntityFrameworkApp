84a20e0bc5cc93d26e06d269dcb4370a5f06c1d6
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-07 12:33:35 +0300

    EntityFramework

--- EntityFrameworkApp/Program.cs
+++ EntityFrameworkApp/Program.cs
@@ -14,6 +14,7 @@ namespace EntityFrameworkApp
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
